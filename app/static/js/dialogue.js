b6e74f5ef11d8e0950b575fa4e6ab9a0b26c8ddb
ClemMourot <clem.mourot@hotmail.fr>
2019-04-02 18:09:11 +0200

    Css style added for inputs and answers
    + marker set to location
    + dialogue.js updated
    + get_data.js created

--- app/static/js/dialogue.js
+++ app/static/js/dialogue.js
@@ -1,32 +1,60 @@
-function createAnswer(){
+function displayInput(){
+
     var bubble = document.createElement("div");
     bubble.classList.add("bubble");
 
     var bubbleText = document.createElement("div");
-    bubbleText.classList.add("bubble-text");
+    bubbleText.classList.add("bubble-text", "bubble-text-input");
+
+    var input = document.createElement("h4");
+    var userInput = document.getElementById("question").value;
+    input.textContent = userInput;
+
+    bubbleText.appendChild(input);
+
+    bubble.appendChild(bubbleText);
+
+    return bubble;
+};
+
+function displayAnswer(){
+
+    var bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+
+    var bubbleText = document.createElement("div");
+    bubbleText.classList.add("bubble-text", "bubble-text-answer");
 
     var answer = document.createElement("h4");
-    answer.textContent = "blabla";
+    answer.textContent = "blbala";
 
     bubbleText.appendChild(answer);
 
     bubble.appendChild(bubbleText);
 
-    var answers = document.getElementById("answers");
-    answers.appendChild(bubble);
+    return bubble;
 };
 
-function generateResponse() {
-    var input = document.getElementById('question');
+function display() {
+    var input = document.getElementById("question");
 
-    input.addEventListener('keyup',function(e){
-        if (e.keyCode === 13) {
-        createAnswer();
-  }
-});
-
+    if (input){
+        input.addEventListener("keyup",function(e){
+            if (e.keyCode === 13) {
+                var answers = document.getElementById("answers");
+                var inputBubble = displayInput();
+                answers.appendChild(inputBubble);
+                input.value = "";
+                var answerBubble = displayAnswer();
+                answers.appendChild(answerBubble);
+                setLocation(35.689487, 139.691706);
+            }
+        });
+    }
 };
 
+
+window.onload = function() { display();  };
 
 
 
--- app/static/js/google_maps.js
+++ app/static/js/google_maps.js
@@ -1,4 +1,5 @@
 let map;
+let marker;
 let coordinates = {lat: 48.866667, lng: 2.333333};
 
 function initMap() {
@@ -8,12 +9,20 @@ function initMap() {
 	};
 	map = new google.maps.Map(document.getElementById('map'), options);
 
-	let marker = new google.maps.Marker({
+	marker = new google.maps.Marker({
 		position: coordinates,
 		map: map,
 		title: 'Paris'
 	});
 	marker.setMap(map);
+};
 
-}
 google.maps.event.addDomListener(window, "load", initialize);
+
+function setLocation(latitude, longitude) {
+    coordinates = {lat: latitude, lng: longitude};
+    marker.setPosition(coordinates);
+    map.panTo(coordinates);
+    map.setZoom(10);
+};
+
