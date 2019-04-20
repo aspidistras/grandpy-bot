627f0d684ac75d80a547236f37de58acaa4bb47c
ClemMourot <clem.mourot@hotmail.fr>
2019-04-20 18:20:14 +0200

    Constants updated

--- constants.py
+++ constants.py
@@ -1,11 +1,6 @@
 """initializes the constants needed throughout the app"""
 
 
-import os
-
-from boto.s3.connection import S3Connection
-
-
 URL_WIKI = "https://fr.wikipedia.org/w/api.php?action=query" \
             "&prop=extracts&explaintext=true&exsectionformat=plain" \
             "&exsentences=5&format=json&formatversion=2&titles={}"
@@ -13,5 +8,4 @@ URL_WIKI = "https://fr.wikipedia.org/w/api.php?action=query" \
 URL_MAPS = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}"
 
 
-GOOGLE_MAPS_KEY = S3Connection(os.environ['KEY_ID'], os.environ['GOOGLE_MAPS_KEY'])
-# connecting to Heroku's environment variables
+GOOGLE_MAPS_KEY = "AIzaSyA9g664fFNDknx_Btz0JLoVMw-4uYHXzK8"
